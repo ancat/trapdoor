32934114c5b6b2884680bcb6a34fa53efce4443b
ancat <946975+ancat@users.noreply.github.com>
2022-11-08 22:32:43 -0500

    make it opt in

--- lib/smuggle-env.rb
+++ lib/smuggle-env.rb
@@ -1,16 +1,30 @@
-ENV.instance_eval do 
-  PUBLIC_ENV = ENV.method(:[])
-  KEYS = ENV.method(:keys)
+ENV.instance_eval do
+  OLD_ASSIGN = ENV.method(:[]=)
+  OLD_HASH = ENV.method(:[])
+  OLD_TO_H = ENV.method(:to_h)
   SMUGGLED = {}
+  @smuggling = false
+
+  def start_smuggling(keys: [])
+    keys.each { |key|
+      hide(key)
+    }
+
+    @smuggling = true
+  end
 
   def [](key)
-    value = PUBLIC_ENV.call key
+    value = OLD_HASH.call key
     return value unless value.nil?
     return SMUGGLED[key]
   end
 
   def []=(key, value)
-    SMUGGLED[key] = value
+    if @smuggling
+      SMUGGLED[key] = value
+    else
+      OLD_ASSIGN.call(key, value)
+    end
   end
 
   def smuggle(key, value)
@@ -18,12 +32,14 @@ ENV.instance_eval do
   end
 
   def hide(key)
-    SMUGGLED[key] = PUBLIC_ENV.call key
+    SMUGGLED[key] = OLD_HASH.call key
     ENV.delete key
   end
 
   def to_h
-    ENV.map{|k,v| [k,v]}.to_h.merge(SMUGGLED.map{|k,v| [k,"**REDACTED**"]}.to_h)
+    OLD_TO_H.call.merge(SMUGGLED.map { |k,v|
+      [k, "**REDACTED**"]
+    }.to_h)
   end
 
   def inspect
