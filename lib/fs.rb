4c934bc256e72ad7b466b27fb57876de1466962c
ancat <946975+ancat@users.noreply.github.com>
2023-01-22 16:29:46 -0500

    don't open a file every single time

--- lib/fs.rb
+++ lib/fs.rb
@@ -1,13 +1,21 @@
 class Secret_FS
   def initialize(base:)
     @base = base
+    @cache = {}
   end
 
   def [](key)
-    open("#{@base}/#{key.downcase}.txt").read
+    return @cache[key] if @cache.include? key
+    return @cache[key] = get_secret(secret: key)
   end
 
   def []=(key, value)
     raise "NOOOOOOOOOO"
+  end
+
+  private
+
+  def get_secret(secret:)
+    open("#{@base}/#{secret.downcase}.txt").read
   end
 end
